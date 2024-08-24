68f44c5c37a3ec31d9e547b0fca400773758fe83
Nailyn DFl <nailyn.diazf@gmail.com>
2024-08-23 21:13:32 -0500

    Add variable in withdraw method

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -7,34 +7,29 @@ public class BankAccount {
     private double balance;
 
     public BankAccount(Scanner sc, Random random){
-        System.out.println("Please, create your bank account username");
+        System.out.println("Welcome to Hello Kitty Bank! \nPlease, enter your bank account username");
         this.accountHolderName = sc.next();
         this.accountNumber = random.nextInt(10000);
         this.balance = 0;
-        System.out.println("Your bank account has been successfully created! \nUsername: ".concat(accountHolderName)+"\nAccount number: " + accountNumber);
+        System.out.println("Your Hello Kitty bank account has been successfully created! \nUsername: ".concat(accountHolderName)+"\nAccount number: " + accountNumber);
     }
-    public void deposit(){
-        Scanner scAmount = new Scanner(System.in);
+    public void deposit(Scanner sc){
         System.out.println("Please, enter the deposit into your account");
-        double depositAmount = scAmount.nextDouble();
-        balance += depositAmount;
+        balance +=  sc.nextDouble();
         System.out.println("Your new balance is: " + balance);
     }
-    public double withdraw(double withdrawAmount){
-        Scanner scWithdraw = new Scanner(System.in);
-        System.out.println("Please, enter the amount of money you want to withdraw from you account");
-        withdrawAmount = scWithdraw.nextDouble();
-        if (balance>= withdrawAmount) {
-            balance = balance - withdrawAmount;
+    public void withdraw(Scanner sc){
+        System.out.println("Please, enter the amount of money you want to withdraw from your account");
+        double withdrawalAmount = sc.nextDouble();
+        if (balance>= withdrawalAmount) {
+            balance = balance - withdrawalAmount;
+            System.out.println("Your new balance is: " + balance);
         }else {
-            System.out.println("Transaction unsuccessful. Insufficient funds");
+            System.out.println("Transaction unsuccessful. Insufficient funds" + balance);
         }
-        System.out.println("Your new balance is: " + balance);
-        return balance;
     }
     public double checkBalance() {
         System.out.println("Your balance is: " + balance);
-//        this.deposit(100);
         return balance;
     }
 
@@ -43,7 +38,8 @@ public class BankAccount {
         Random random = new Random();
 
         BankAccount firstUser = new BankAccount(sc, random);
-        firstUser.deposit();
+        firstUser.deposit(sc);
+        firstUser.withdraw(sc);
     }
 
 }
